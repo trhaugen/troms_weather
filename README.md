9cf17f9596c7c5f485a1b0e4b1218a26ee10523c
Tonje Haugen <tonje.reinholdt@gmail.com>
2025-03-20 12:17:39 +0100

    Added documentation and updated readme

--- README.md
+++ README.md
@@ -3,11 +3,26 @@
 
 This is a project looking at the weather of Tromsø and the surrounding area to look at how it's changed over the years, how long the data has been collected and how the quality of the data has changed over time. More to come later
 
-## Requirements
 
-This is a python project and needs this modules in addition to python 
+## Installation
 
+This project requires specific modules to run properly. Below are the instructions to set up the environment using **conda**. If you want to use something else
+the packages and their version can be found in the *environment.yml* file.
 
+To set up the environment, you need to have [Anaconda](https://www.anaconda.com/products/distribution) or [Miniconda](https://docs.conda.io/en/latest/miniconda.html) installed.
+
+### 1. Create the Environment
+
+Run the following command in your terminal to create the conda environment where it will be named **troms_weather**:
+```
+conda env create -f environment.yml
+````
+Activate said environment with the code:
+````
+conda activate troms_weather
+```
+
+You can now run all the scripts and files in the scrips folder 
 ## How to use 
 
 
--- scripts/reading_data.py
+++ scripts/reading_data.py
@@ -6,15 +6,27 @@ import requests
 import sys
 
 class ReadingData:
+    """
+    Reads data from the FROST API for a given station. 
+
+    Args:
+        station_name (str): The station name.
+        client_id (str): The client id for the FROST API.
+        client_secret (str): The client secret for the FROST API.
+    """
     def __init__(self, station_name: str, client_id: str, client_secret: str):
         self.station_name = station_name
         with open (client_id) as f: self.client_id = f.read().strip()
         with open (client_secret) as f: self.client_secret = f.read().strip()
-
-    def read_csv(self):
-        return pd.read_csv(self.file_path)
     
-    def status_code(self, r):
+    def status_code(self, r) -> None:
+        """
+        Checks the status code for the requested data and exists the system if 
+        something is wrong. 
+
+        Args:
+            r (requests.models.Response): The response object from the request.
+        """
         if r.status_code == 200:
             pass
         else:
@@ -23,7 +35,17 @@ class ReadingData:
             print('Reason: %s' % r.json()['error']['reason'])
             sys.exit()     
 
-    def get_station_info(self):
+    def get_station_info(self) -> tuple:
+        """
+        Fetches metadata information for a weather station from the Frost API.
+
+        Returns:
+            tuple: A tuple containing:
+                - valid_from (str): The start date of the station's validity period (YYYY-MM-DD).
+                - valid_to (str): The end date of the station's validity period, or 'Ongoing' if not specified.
+                - coordinates (list): The station's geographic coordinates [longitude, latitude, altitude].
+                - short_name (str): The short name of the weather station.
+        """
         endpoint = 'https://frost.met.no/sources/v0.jsonld'
         parameters = {
             'ids': self.station_name,
@@ -40,7 +62,23 @@ class ReadingData:
         short_name = jreq['data'][0]['shortName']
         return valid_from, valid_to, coordinates, short_name
 
-    def get_station_datatypes(self, reference_time: str = '2025'):
+    def get_station_datatypes(self, reference_time: str = '2025') -> set:
+        """
+        Fetches the available observation data types for a given weather station.
+
+        This method queries the Frost API to retrieve all available data types 
+        (e.g., temperature, precipitation) that have been recorded at the 
+        specified weather station.
+
+        Args:
+        reference_time (str, optional): The year for which to check available 
+        data types. Defaults to '2025'.
+
+        Returns:
+            set: A set containing all available data types (element IDs) for the
+            given station.
+
+        """
         endpoint = 'https://frost.met.no/observations/availableTimeSeries/v0.jsonld'
         parameters = {
             'sources': self.station_name,
@@ -52,13 +90,17 @@ class ReadingData:
         all_datatypes = set([jreq['data'][i]['elementId'] for i in range(len(jreq['data']))])
         return all_datatypes
     
-    def get_temperature_daily(self, start_date: str, end_date: str):
+    def get_temperature_daily(self, start_date: str, end_date: str) -> pd.DataFrame:
         """
         Gets the daily mean air temperature for a given time period from the FROST
         API. Returns a DataFrame with the date and temperature for the given station.
-        Parameters:
+
+        Args:
         start_date (str): Start of time period for gathering data in the format 'YYYY-MM-DD'
         end_date (str): End of time period for gathering data in the format 'YYYY-MM-DD'
+
+        Returns:
+        pd.DataFrame: DataFrame with the date and daily temperature for given timeperiod
         """
         endpoint ='https://frost.met.no/observations/v0.jsonld'
         parameters = {
@@ -75,14 +117,19 @@ class ReadingData:
         df = pd.DataFrame({'date': c_dates, 'temperature': temperature})
         return df
     
-    def get_temperature_yearly(self, start_year:str, end_year:str):
+    def get_temperature_yearly(self, start_year:str, end_year:str) -> pd.DataFrame:
         """
         Gets the yearly mean air temperature anomaly for a given time period 
         with respect to the period 1961-1990 from the FROST API. Returns a
         DataFrame with the year and temperature anomaly for the given station.
-        Parameters:
-        start_year (str): Start of time period for gathering data in the format 'YYYY'
-        end_year (str): End of time period for gathering data in the format 'YYYY'
+
+        Args:
+            start_year (str): Start of time period for gathering data in the format 'YYYY'
+            end_year (str): End of time period for gathering data in the format 'YYYY'
+        
+        Returns:
+            pd.DataFrame: DataFrame with the year, min, max and mean yearly temperature 
+            for given timeperiod.
         """
         endpoint = 'https://frost.met.no/observations/v0.jsonld'
         parameters = {
@@ -93,21 +140,28 @@ class ReadingData:
         req = requests.get(endpoint, parameters, auth=(self.client_id, ''))
         self.status_code(req)
         jreq = req.json()
-        # Create a DataFrame with the year, min, max and mean temperature
         df = pd.DataFrame([{
             'year': datetime.datetime.strptime(entry['referenceTime'], "%Y-%m-%dT%H:%M:%S.%fZ").year,
             'min': next((obs.get('value') for obs in entry['observations'] if obs.get('elementId') == 'min(air_temperature P1Y)'), None),
             'max': next((obs.get('value') for obs in entry['observations'] if obs.get('elementId') == 'max(air_temperature P1Y)'), None),
             'mean': next((obs.get('value') for obs in entry['observations'] if obs.get('elementId') == 'mean(air_temperature P1Y)'), None)
         } for entry in jreq['data']])
-        # Group by year and create new columns for min, max and mean, adding NaN where no data is available
         df = df.groupby('year', as_index=False).agg({
             'min': 'min',
             'max': 'max',
             'mean': 'mean'})
         return df
     
-    def get_temperature_anomaly(self, start_year:str, end_year:str):
+    def get_temperature_anomaly(self, start_year:str, end_year:str) -> pd.DataFrame:
+        """
+        Gets the monthly mean air temperature anomaly for a given time period
+        with respect to the period 1961-1990 from the FROST API.
+        Args:
+            start_year (str): Start of time period for gathering data in the format 'YYYY'
+            end_year (str): End of time period for gathering data in the format 'YYYY'
+        Returns:
+            pd.DataFrame: DataFrame with the year, month and temperature anomaly for given timeperiod.
+        """
         endpoint = 'https://frost.met.no/observations/v0.jsonld'
         parameters = {
             'sources': self.station_name,
@@ -117,7 +171,6 @@ class ReadingData:
         req = requests.get(endpoint, parameters, auth=(self.client_id, ''))
         self.status_code(req)
         jreq = req.json()
-        # Create a DataFrame with the year and quatertly temperature anomaly
         df = pd.DataFrame([{
             'year': datetime.datetime.strptime(entry['referenceTime'], "%Y-%m-%dT%H:%M:%S.%fZ").year,
             'month': datetime.datetime.strptime(entry['referenceTime'], "%Y-%m-%dT%H:%M:%S.%fZ").month,
@@ -133,6 +186,13 @@ class FindingStations:
         with open (client_secret) as f: self.client_secret = f.read().strip()
 
     def status_code(self, r):
+        """ 
+        Checks the status code for the requested data and exists the system if
+        something is wrong.
+
+        Args:
+            r (requests.models.Response): The response object from the request.
+        """
         if r.status_code == 200:
             pass
         else:
@@ -141,7 +201,20 @@ class FindingStations:
             print('Reason: %s' % r.json()['error']['reason'])
             sys.exit()     
 
-    def get_sensors(self, lat: float, lon: float, max_count: int, municipality: str = None):
+    def get_sensors(self, lat: float, lon: float, max_count: int, municipality: str = None) -> list:
+        """
+        Fetches sensors from the FROST API based on the given latitude and longitude. 
+        If municipality is specified, only sensors from that municipality are returned.
+        
+        Args:
+            lat (float): The latitude of the location.
+            lon (float): The longitude of the location.
+            max_count (int): The maximum number of sensors to return.
+            municipality (str): The municipality to filter the sensors by.
+            
+        Returns:
+            list: A list of sensors with the sensor ID and name.
+        """
         endpoint = 'https://frost.met.no/sources/v0.jsonld'
         parameters = {
             'ids': 'SN*',
